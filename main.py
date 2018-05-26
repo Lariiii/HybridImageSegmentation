203932ff8ca559b37ecce886f03e36465a7ac29f
Marvin Thiele <MarvinThiele@gmx.net>
2018-05-26 14:50:52 +0200

    DEM

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def main():
+demPixles = []
 
+def main():
     # Data Reading
     fp = open('resources/DEM.txt', 'r')
+    lines = fp.readlines()
+    cnt = 1
 
+    while line:
+        print("Line {}: {}".format(cnt, line.strip()))
+        line = fp.readline()
+        cnt += 1
 
 
 if __name__ == "__main__":
