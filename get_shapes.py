2ee3b03491f0631be5f38b57038d4f4f2eed25c2
mirko.krause <mirko.krause@student.hpi.de>
2018-05-26 23:46:35 +0200

    refactored contouring

--- get_shapes.py
+++ get_shapes.py
@@ -27,20 +27,18 @@ def findContoursCV(edges, adaptive=True):
     image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     return image, contours, hierarchy
 
-def drawContoursCV(image, contours, hierarchy):
-    #for c in contours:
+def drawContoursCV(image, contours, hierarchy=None, name="Image"):
     cv.drawContours(
         image=image,
         contours=contours,
         contourIdx=0,
         color=(0,255,0),
         thickness=0)
-    cv.imshow("Image", image)
+    cv.imshow(name, image)
     cv.waitKey(0)
-#    break
 
-def run():
-    im = cv.imread('results/dem.png')
+def run(imageFile='results/dem.png'):
+    im = cv.imread(imageFile)
     edges = edgeDetection(im)
     image, contours, hierarchy = findContoursCV(edges)
-    drawContoursCV(image, contours, hierarchy)
+    drawContoursCV(image, contours, hierarchy, name=imageFile)
