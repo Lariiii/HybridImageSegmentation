983e840f8ad8f30d8dab93a540f63329d2ee710b
mirko.krause <mirko.krause@student.hpi.de>
2018-05-26 23:36:54 +0200

    refactored contouring

--- get_shapes.py
+++ get_shapes.py
@@ -1,30 +1,48 @@
 import matplotlib.pyplot as plt
-import cv2
+import cv2 as cv
 
-def edgeDetection(image):
-    edges = cv2.Canny(image,100,200)
-    plt.imshow(edges)
-    plt.xticks([]), plt.yticks([])
-    plt.savefig('results/edge')
+def edgeDetection(image, show=True, outputname='results/edge'):
+    edges = cv.Canny(image,100,200)
+    if show:
+        plt.imshow(edges)
+        plt.xticks([]), plt.yticks([])
+
+    if outputname is not None: plt.savefig(outputname)
+
     return edges
 
-def findContoursCV(edges):
-#    ret, thresh = cv2.threshold(edges,127,255, 0)
-#    thresh = cv2.adaptiveThreshold(edges,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-    thresh = cv2.adaptiveThreshold(edges,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,0)
-    image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+def findContoursCV(edges, adaptive=True):
+    if adaptive:
+        # https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html
+        thresh = cv.adaptiveThreshold(
+            src=edges,
+            maxValue=255,
+            adaptiveMethod=cv.ADAPTIVE_THRESH_GAUSSIAN_C,  #cv.ADAPTIVE_THRESH_MEAN_C
+            thresholdType=cv.THRESH_BINARY_INV,
+            blockSize=11,
+            C=0)
+    else:
+        _, thresh = cv.threshold(edges, 127, 255, 0)
+
+    image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     return image, contours, hierarchy
 
 def drawContoursCV(image, contours, hierarchy):
-    for c in contours:
-        cv2.drawContours(image, [c], -1, (0,255,0), 2)
-        cv2.imshow("Image", image)
-        cv2.waitKey(0)
-        break
+    #for c in contours:
+    cv.drawContours(
+        image=image,
+        contours=contours,
+        contourIdx=0,
+        color=(0,255,0),
+        thickness=0)
+    cv.imshow("Image", image)
+    cv.waitKey(0)
+#    break
 
 def run():
-    im = cv2.imread('results/dem.png')
+    im = cv.imread('results/dem.png')
     edges = edgeDetection(im)
     image, contours, hierarchy = findContoursCV(edges)
     drawContoursCV(image, contours, hierarchy)
 
+run()
