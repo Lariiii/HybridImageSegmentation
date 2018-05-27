e3535152f1a4ab6ac1981a4c0055e97bcb96567f
Marvin Thiele <MarvinThiele@gmx.net>
2018-05-27 06:24:08 +0200

    Small change

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def main():
         dataframeToImage(read_subjective2(), 'results/subjective2.png')
         dataframeToImage(read_corine(), 'results/corine.png')
 
-        dataframeToImage(pruning(read_aspect1()), True, 'results/aspect1.png')
-        dataframeToImage(pruning(read_aspect2()), True, 'results/aspect2.png')
+        dataframeToImage(pruning(read_aspect1()), 'results/aspect1.png')
+        dataframeToImage(pruning(read_aspect2()), 'results/aspect2.png')
 
-        dataframeToImage(pruning(read_dem()), True, 'results/dem.png')
-        dataframeToImage(pruning(read_ndvi()), True, 'results/ndvi.png')
-        dataframeToImage(pruning(read_slope()), True, 'results/slope.png')
+        dataframeToImage(pruning(read_dem()), 'results/dem.png')
+        dataframeToImage(pruning(read_ndvi()), 'results/ndvi.png')
+        dataframeToImage(pruning(read_slope()), 'results/slope.png')
 
     if shapeMatching:
         approach_shapeMatching.run()
