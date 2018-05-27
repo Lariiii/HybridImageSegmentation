181e6476f2123f3d1b7c72756ba6abd18b242a2e
Lariiii <lari-lari@gmx.net>
2018-05-27 04:42:43 +0200

    not much

--- match_color.py
+++ match_color.py
@@ -5,17 +5,18 @@ from get_shapes import edgeDetection, findContoursCV, drawContoursCV
 
 subjective_1 = cv2.imread('results/subjective1.png')
 subjective_2 = cv2.imread('results/subjective2.png')
+filename = 'test'
 
-def dataframeToImageColorMerge(dataframe1, dataframe2, classRangeHigh, coninuus, filename):
+def dataframeToImageColorMerge(dataframe1, dataframe2, classRangeHigh, filename):
 
     min_x = 1000000
     max_x = 0
     min_y = 1000000
     max_y = 0
 
-    for i, row in dataframe.iterrows():
-        dataframe.set_value(i, 'x', row['x'] / 100)
-        dataframe.set_value(i, 'y', row['y'] / 100)
+    for i, row in dataframe1.iterrows():
+        dataframe1.set_value(i, 'x', row['x'] / 100)
+        dataframe1.set_value(i, 'y', row['y'] / 100)
         if row['x'] < min_x:
             min_x = row['x']
         if row['x'] > max_x:
@@ -33,23 +34,21 @@ def dataframeToImageColorMerge(dataframe1, dataframe2, classRangeHigh, coninuus,
     data = numpy.zeros((431, 337, 3), dtype=numpy.uint8)
 
     colors = []
-    if coninuus:
-        # 0 to 1 or 0 to 600
-        for i, row in dataframe.iterrows():
-            data[int(row['x'])][int(row['y'])] = [int((row['class'] / classRangeHigh) * 255), 0, 128]
-    else:
-        for i in range(classRangeHigh):
-            colors.append([numpy.random.randint(low=0, high=255), numpy.random.randint(low=0, high=255),
-                           numpy.random.randint(low=0, high=255)])
+    for i in range(classRangeHigh):
+        colors.append([numpy.random.randint(low=0, high=255), numpy.random.randint(low=0, high=255),
+                       numpy.random.randint(low=0, high=255)])
 
-        for i, row in dataframe.iterrows():
-            # print(""+str(row['x'])+" "+str(row['y']))
-            print(int(row['class']) - 1)
-            data[int(row['x'])][int(row['y'])] = colors[int(row['class']) - 1]
+# über beide dataframes iterieren
+    for i, row in dataframe.iterrows():
+        # print(""+str(row['x'])+" "+str(row['y']))
+        print(int(row['class']) - 1)
+        #if gleiche farben dann einfärben
+        data[int(row['x'])][int(row['y'])] = colors[int(row['class']) - 1]
 
     image = Image.fromarray(data)
     image.save(filename)
 
+dataframeToImageColorMerge(subjective_1, subjective_2, 12, filename)
 
 
 #edges2 = edgeDetection(im2)
