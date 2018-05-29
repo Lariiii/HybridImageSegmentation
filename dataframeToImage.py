47ef4775e9e4dbc91c0cef1d67dc91ee7b302d88
Marvin Thiele <MarvinThiele@gmx.net>
2018-05-29 11:16:26 +0200

    Comment dataFrameToImage

--- dataframeToImage.py
+++ dataframeToImage.py
@@ -4,11 +4,14 @@ import numpy
 from PIL import Image
 
 def dataframePreprocessing(dataframe):
+    # Setting Values to find the max and min values
     min_x = 1000000
     max_x = 0
     min_y = 1000000
     max_y = 0
-
+    
+    # Make the Values smaller by dividing by 100 (since all coordinates are a multiple of 100)
+    # Find Mínd and Max Values to normalize the coorindate into pixels correctly
     for i, row in dataframe.iterrows():
         dataframe.set_value(i, 'x', row['x'] / 100)
         dataframe.set_value(i, 'y', row['y'] / 100)
@@ -21,6 +24,7 @@ def dataframePreprocessing(dataframe):
         if row['y'] > max_y:
             max_y = row['y']
 
+    # Normalize the coordainates
     for i, row in dataframe.iterrows():
         dataframe.set_value(i, 'x', row['x'] - min_x)
         dataframe.set_value(i, 'y', row['y'] - min_y)
@@ -28,24 +32,32 @@ def dataframePreprocessing(dataframe):
     return dataframe
 
 def dataframeToImage(dataframe, filename):
-    #ToDO: Make Jannik Happy
+    #Take a raw dataframe and build an image from that
+    
+    # Find the highest value the "class" field can have
     classRangeHigh = 0
     for i, row in dataframe.iterrows():
         if int(row['class']) > classRangeHigh:
             classRangeHigh = int(row['class'])
 
-    # Not True in general, but for this PoC
+    # This checks wether you have a value range over 42 which means we will render it in some scale rather than categorical coloring
+    # This is also valid if the class range goes from 0 to 1
+    # The Numbers here are not true in general, but sufficient for this PoC
     if classRangeHigh > 42:
         continuous = True
     elif classRangeHigh == 1:
         continuous = True
     else:
         continuous = False
-
+    
+    # Now we will preprocess the data
     dataframe = dataframePreprocessing(dataframe)
 
+    # Create an empty image with RGB in the dimensions of the given data
+    # ToDO: Find these values from the data to generialize this approach
     data = numpy.zeros((431, 337, 3), dtype=numpy.uint8)
-
+    
+    # Generate a random color for each class within the data or create a continous color
     colors = []
     if continuous:
         for i, row in dataframe.iterrows():
@@ -59,5 +71,6 @@ def dataframeToImage(dataframe, filename):
             # print(""+str(row['x'])+" "+str(row['y']))
             data[int(row['x'])][int(row['y'])] = colors[int(row['class']) - 1]
 
+    # Generate the image from the data and save the image
     image = Image.fromarray(data)
     image.save(filename)
